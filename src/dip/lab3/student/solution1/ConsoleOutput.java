d098506218ef73126b402ad13906f8770cf03583
Benjamin <Benjamin@10.30.19.12>
2017-09-28 12:10:41 -0500

    Removed FileInput and FileOutput classes

--- src/dip/lab3/student/solution1/ConsoleOutput.java
+++ src/dip/lab3/student/solution1/ConsoleOutput.java
@@ -5,10 +5,22 @@
  */
 package dip.lab3.student.solution1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Benjamin
  */
-public class ConsoleOutput {
-    
+public class ConsoleOutput implements MessageOutput{
+        /**
+     * Reads characters from the keyboard until a carriage return is entered.
+     *
+     * @return a String representing one line of input.
+     */
+    @Override
+    public String readln() {
+        System.out.println("Enter some text, then press return:");
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
 }
